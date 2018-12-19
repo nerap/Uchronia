10de1e976b06108292c217e81272504d8cfddd65
Adrien <napalm_ground@hotmail.com>
2018-12-19 15:27:37 +0100

    Intégration du projet Unity de base

---
+++ Assets/TileSetting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileSetting : MonoBehaviour
+{
+    public Vector3Int tilePosition = new Vector3Int(4, 4, 1);
+    public Tilemap highlightTileMap;
+    public Tile highlightTile;
+
+    void Start()
+    {
+        highlightTileMap.SetTile(tilePosition, highlightTile);
+    }
+
+    void Update()
+    {
+
+    }
+}
